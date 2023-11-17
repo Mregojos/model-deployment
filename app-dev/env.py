dd0de473270c5071f5f34800120e1075000ab147
Matt <jupyter@matt-nb.us-central1-a.c.matttrainings.internal>
2023-11-17 07:32:12 +0000

    Add and modify files

--- app-dev/env.py
+++
@@ -1,7 +0,0 @@
-# DB Credentials
-DBNAME="matt"
-USER='matt'
-HOST= '34.172.212.171'
-PORT='5000'
-PASSWORD='password'
-PROJECT_NAME="matt-project-training"
--- app-model-dev/app-dev/env.sh
+++
@@ -1,8 +0,0 @@
-DBNAME='matt'
-USER='matt' 
-HOST='34.135.111.54' 
-DBPORT='5000'
-DBPASSWORD='password' 
-PROJECT_NAME='matttrainings'
-ADMIN_PASSWORD='password'
-
