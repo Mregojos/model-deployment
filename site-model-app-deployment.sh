07d7f74eed5a72781961072ffcf04e5773804b9e
Matt <mattiregojos@gmail.com>
2023-11-18 08:00:01 +0000

    Add and modify files

--- site-model-app-deployment.sh
+++ site-model-app-deployment.sh
@@ -8,8 +8,14 @@
 gcloud services enable iam.googleapis.com cloudbuild.googleapis.com artifactregistry.googleapis.com run.googleapis.com aiplatform.googleapis.com cloudresourcemanager.googleapis.com
 echo "\n #----------Services have been successfully enabled.----------# \n"
 
-# Environment Variables
-DB_NAME="db"
+#----------Local Development Environment Variables----------#
+VERSION="ii"
+APP_NAME="site-model-app-dev-$VERSION"
+FIREWALL_RULES_NAME="ports"
+INSTANCE_NAME="matt"
+
+#----------Database Instance Environment Variables----------#
+DB_INSTANCE_NAME="db"
 MACHINE_TYPE="e2-micro"
 REGION="us-west1"
 ZONE="us-west1-a"
@@ -17,9 +23,22 @@ BOOT_DISK_SIZE="30"
 TAGS="db"
 FIREWALL_RULES_NAME="ports"
 STATIC_IP_ADDRESS_NAME="db-static-ip-address"
-INSTANCE_NAME="matt"
 
-# Change the APP_NAME
+#---------Database Credentials----------#
+DB_CONTAINER_NAME='postgres-sql'
+DB_NAME='matt'
+DB_USER='matt' 
+DB_HOST=$(gcloud compute instances list --filter="name=$INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)") 
+DB_PORT=5000
+DB_PASSWORD='password'
+PROJECT_NAME='$(gcloud config get project)'
+ADMIN_PASSWORD=password
+APP_PORT=9000
+APP_ADRESS=
+DOMAIN_NAME=
+SPECIAL_NAME='Matt'
+
+#----------Deployment Environment Variables----------#
 VERSION="i"
 APP_NAME="site-model-app-deployment-$VERSION"
 CLOUD_BUILD_REGION="us-west2"
@@ -66,15 +85,15 @@ cd site-model-app-deployment
 
 # Environment Variables for the app
 echo """
-DBNAME:
+DB_NAME:
     'matt'
-USER:
+DB_USER:
     'matt'
-HOST:
+DB_HOST:
     '$(gcloud compute instances list --filter="name=$INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")'
-DBPORT:
+DB_PORT:
     '5000'
-DBPASSWORD:
+DB_PASSWORD:
     'password'
 PROJECT_NAME:
     '$(gcloud config get project)'
@@ -86,6 +105,8 @@ APP_ADDRESS:
     ''
 DOMAIN_NAME:
     ''
+SPECIAL_NAME:
+    'Matt'
 """ > env.yaml
 
 
