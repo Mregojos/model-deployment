9da500125e10b83429e9fa88f4c58aaa4b1b22eb
Matt <mattiregojos@gmail.com>
2023-11-22 06:36:37 +0000

    Add and modify files

--- versions/site-model-version-iii/site-model-app-dev.sh
+++ versions/site-model-version-iii/site-model-app-dev.sh
@@ -9,42 +9,45 @@
 gcloud services enable iam.googleapis.com cloudbuild.googleapis.com artifactregistry.googleapis.com run.googleapis.com aiplatform.googleapis.com cloudresourcemanager.googleapis.com
 echo "\n #----------Services have been successfully enabled.----------# \n"
 
-#----------Environment Variables----------#
-VERSION="iii"
-APP_NAME="site-model-app-dev-$VERSION"
-FIREWALL_RULES_NAME="ports"
-INSTANCE_NAME="matt"
+#----------Environment Variables for local development----------#
+VERSION="viii" # Change this
+# APP_NAME="site-model-app-dev-$VERSION" # Development
+# FIREWALL_RULES_NAME="ports"
+# INSTANCE_NAME=""
+
+#----------Database Instance Environment Variables----------#
+APP_NAME="site-model-app-deployment-$VERSION" # Deployment
+DB_INSTANCE_NAME="$APP_NAME-db"
 
 #---------Database Credentials----------#
-DB_CONTAINER_NAME='postgres-sql'
-DB_NAME='matt'
-DB_USER='matt' 
-DB_HOST=$(gcloud compute instances list --filter="name=$INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)") 
+DB_CONTAINER_NAME="$APP_NAME-postgres-sql"
+# DB_NAME="$APP_NAME-db"
+DB_USER="$APP_NAME-admin" 
+DB_HOST=$(gcloud compute instances list --filter="name=$DB_INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)")
 DB_PORT=5000
-DB_PASSWORD='password'
+DB_PASSWORD='password' # change the value in production 
 PROJECT_NAME='$(gcloud config get project)'
-ADMIN_PASSWORD=password
+ADMIN_PASSWORD=password # change the value in production
 APP_PORT=9000
-APP_ADRESS=
-DOMAIN_NAME=
-SPECIAL_NAME='Matt'
+APP_ADRESS= # change the value in production
+DOMAIN_NAME= # change the value in production
+SPECIAL_NAME='Matt' # change the value in production
 
-
-#----------Database----------#
+#----------Database for local development----------#
 # With volume/data connected
 # cd app-model 
 # cd app-model
-docker run -d \
-    --name $DB_CONTAINER_NAME \
-    -e POSTGRES_USER=$DB_USER \
-    -e POSTGRES_PASSWORD=$DB_PASSWORD \
-    -v $(pwd)/data/:/var/lib/postgresql/data/ \
-    -p $DB_PORT:5432 \
-    postgres
-docker run -p 8000:80 \
-    -e 'PGADMIN_DEFAULT_EMAIL=matt@example.com' \
-    -e 'PGADMIN_DEFAULT_PASSWORD=password' \
-    -d dpage/pgadmin4
+# docker run -d \
+#    --name $DB_CONTAINER_NAME \
+#    -e POSTGRES_USER=$DB_USER \
+#    -e POSTGRES_PASSWORD=$DB_PASSWORD \
+#    -v $(pwd)/data/:/var/lib/postgresql/data/ \
+#    -p $DB_PORT:5432 \
+#    postgres
+# docker run -p 8000:80 \
+#    -e 'PGADMIN_DEFAULT_EMAIL=matt@example.com' \
+#    -e 'PGADMIN_DEFAULT_PASSWORD=password' \
+#    -d dpage/pgadmin4
     
 #----------Local Development----------#
 # ***** Use with container instead of this 
@@ -67,23 +70,23 @@ docker run -p 8000:80 \
 # export DBPASSWORD='password' 
 # export PROJECT_NAME='$(gcloud config get project)'
 
-#----------Local Development using container----------#
+#----------Local Development using container with deployment environment variables----------#
 # For App Development
 cd site-model-app-dev
 
 # Environment Variables for the app
-echo """DB_NAME='matt'
-DB_USER='matt' 
-DB_HOST=$(gcloud compute instances list --filter="name=$INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)") 
-DB_PORT=5000
-DB_PASSWORD='password'
+echo """DB_NAME=$DB_USER
+DB_USER=$DB_USER 
+DB_HOST=$DB_HOST
+DB_PORT=$DB_PORT
+DB_PASSWORD=$DB_PASSWORD
 PROJECT_NAME='$(gcloud config get project)'
-ADMIN_PASSWORD=password
-APP_PORT=9000
+ADMIN_PASSWORD=$ADMIN_PASSWORD
+APP_PORT=$APP_PORT
 APP_ADRESS=
 DOMAIN_NAME=
-SPECIAL_NAME='Matt'
-""" > env.sh
+SPECIAL_NAME=$SPECIAL_NAME
+""" > .env.sh
 
 # Remove docker container
 # docker rm -f $APP_NAME
@@ -92,11 +95,11 @@ SPECIAL_NAME='Matt'
 docker build -t $APP_NAME .
 
 # Run
-docker run -d -p 9000:9000 -v $(pwd):/app --env-file env.sh --name $APP_NAME $APP_NAME
+docker run -d -p 9000:9000 -v $(pwd):/app --env-file .env.sh --name $APP_NAME $APP_NAME
 
 # Create a firewall (GCP)
-gcloud compute --project=$(gcloud config get project) firewall-rules create $FIREWALL_RULES_NAME \
-    --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:5000,tcp:8000,tcp:9000 --source-ranges=0.0.0.0/0 
+# gcloud compute --project=$(gcloud config get project) firewall-rules create $FIREWALL_RULES_NAME \
+#    --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:5000,tcp:8000,tcp:9000 --source-ranges=0.0.0.0/0 
 # Remove docker container
 # docker rm -f $APP_NAME
 
