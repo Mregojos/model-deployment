9f02335c3458c6ab52c6cdfd5667287f52e8ae70
Matt <mattiregojos@gmail.com>
2023-11-28 07:01:33 +0000

    Add and modify files

--- app-deployment/startup-script.sh
+++ app-deployment/startup-script.sh
@@ -1,7 +1,8 @@
-# Environment Variables
-export VERSION="v" 
-export APP_NAME="app-prod-$VERSION" 
-export DB_PASSWORD="password" 
+# Environment
+# TO DO: In production, change the values to be more secure. 
+export VERSION="v" # Change this
+export APP_NAME="app-prod-$VERSION" # change the value in production 
+export DB_PASSWORD="password" # change the value in production 
 
 export DB_CONTAINER_NAME="$APP_NAME-sql"
 # export DB_NAME="$APP_NAME-admin"
