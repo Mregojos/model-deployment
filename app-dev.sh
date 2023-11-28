2d18ec83235b6b5a0c9ec0b3a2d61ceb6f7f5d33
Matt <mattiregojos@gmail.com>
2023-11-28 06:38:53 +0000

    Add and modify files

--- app-dev.sh
+++ app-dev.sh
@@ -8,7 +8,7 @@ gcloud services enable compute.googleapis.com iam.googleapis.com cloudbuild.goog
 echo "\n #----------Services have been successfully enabled.----------# \n"
 
 # Directory
-cd app-deployment
+# cd app-deployment
 
 #---------Application Name Environment Variables----------#
 # In production, change these values.
