e109800a1c2ec40307a4caf7a021c996bac3c20d
Matt <mattiregojos@gmail.com>
2023-11-29 01:40:47 +0000

    Add and modify files

--- app-deployment/pages/Agent.py
+++ app-deployment/pages/Agent.py
@@ -96,7 +96,7 @@ def sections(con, cur):
         elif login:
             if username == "admin" and password == ADMIN_PASSWORD:
                 credential = True
-                st.write(f":violet[Your chat will be stored in a database, use the same name to see your past conversations.]")
+                st.write(f":violet[Your chat will be stored in a database. Use the same name to see your past conversations.]")
                 st.caption(":warning: :red[Do not add sensitive data.]")
                 model = st.selectbox("Choose Chat or Code Generation?", ('Chat', 'Code'))
                 input_name = st.text_input("Your Name")
@@ -127,7 +127,7 @@ def sections(con, cur):
     #----------For Guest Login----------#            
         elif guest:
             credential = True
-            st.write("You will be my agent's :blue[guest].")
+            st.write("You will be Agent's :blue[guest].")
             st.write(f":violet[Your chat will be stored in a database. Use the same name to see your past conversations.]")
             st.caption(":warning: :red[Do not add sensitive data.]")
             model = st.selectbox("Choose Chat or Code Generation?", ('Chat', 'Code'))
@@ -138,7 +138,7 @@ def sections(con, cur):
                 st.info(f"Your name for this conversation is :blue[{input_name}]")
             elif save and input_name == "":
                 st.info("Save your name first.")
-            agent = st.toggle("**:violet[Let's talk to Agent]**")
+            agent = st.toggle("**:violet[Start the conversation]**")
             time = t.strftime("Date: %Y-%m-%d | Time: %H:%M:%S UTC")
             time_date = time[0:15]
             if agent:
@@ -280,9 +280,9 @@ def sections(con, cur):
     #----------For Guest----------#    
     elif guest and not login:
         if credential is False:
-            st.info("Save your name and toggle the :violet[Let's talk to Agent] to start the conversation.")
+            st.info("Save your name and toggle the :violet[Start the conversation].")
         elif credential is True and agent is False:
-            st.info("Save your name and toggle the :violet[Let's talk to Agent] to start the conversation. Enjoy chatting :smile:")
+            st.info("Save your name and toggle the :violet[Start the conversation]. Enjoy chatting :smile:")
         elif credential is True and agent is True and input_name == "":
             st.info("Don't forget to save your name to continue.")
         elif credential is True and agent is True and total_count < LIMIT:
