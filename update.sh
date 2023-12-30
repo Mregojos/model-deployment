4bea5bda8b4a9c01ee4472cc434f9aff19157a05
Matt <mattiregojos@gmail.com>
2023-12-30 04:16:52 +0000

    Add and modify files

---
+++ update.sh
@@ -0,0 +1,6 @@
+# Update
+cp -rf app-multimodal/Agent.py ~/model-deployment/app-deployment/pages
+cp -rf app-multimodal/Agent.py ~/model-deployment/app-dev/pages
+cp -rf test/* ~/model-deployment/test
+cp -rf makefile ~/model-deployment/makefile
+
