c46a30cfe31dcca30d7a5ea93d94e8d3ffc2d5ee
Matt <mattiregojos@gmail.com>
2023-12-30 04:49:51 +0000

    Add and modify files

--- app-dev.sh
+++ app-dev.sh
@@ -5,6 +5,9 @@ cd app-dev
 # Directory
 # cd app-deployment
 
+# DB_HOST Static IP Address
+DB_HOST=$(gcloud compute instances list --filter="name=$DB_INSTANCE_NAME" --format="value(networkInterfaces[0].accessConfigs[0].natIP)") 
+
 # Environment Variables for the app
 echo """DB_NAME=$DB_USER
 DB_USER=$DB_USER 
@@ -29,8 +32,12 @@ docker build -t $APP_NAME .
 docker run -d -p 9000:9000 -v $(pwd):/app --env-file .env.sh --name $APP_NAME $APP_NAME
 
 # Create a firewall (GCP)
-gcloud compute --project=$(gcloud config get project) firewall-rules create $FIREWALL_RULES_NAME-dev \
-    --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:9000 --source-ranges=0.0.0.0/0 
+if gcloud compute firewall-rules list --filter="name=$FIREWALL_RULES_NAME-dev" --format="table(name)" | grep -q $FIREWALL_RULES_NAME-dev; then
+    echo ""
+else
+    gcloud compute --project=$(gcloud config get project) firewall-rules create $FIREWALL_RULES_NAME-dev \
+        --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:9000 --source-ranges=0.0.0.0/0 
+fi
 
 # Remove docker container
 # docker rm -f $APP_NAME
